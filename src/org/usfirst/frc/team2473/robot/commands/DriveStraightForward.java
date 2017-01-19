0dc2c3815b5e6fca34ae1626c0a93e8914b51740
William Fang <willfang8@yahoo.com>
2017-01-18 20:15:51 -0800

    changed robot back to src and wrote turn + drivestraight

---
+++ src/org/usfirst/frc/team2473/robot/commands/DriveStraightForward.java
@@ -0,0 +1,66 @@
+package org.usfirst.frc.team2473.robot.commands;
+
+import org.usfirst.frc.team2473.robot.Database;
+import org.usfirst.frc.team2473.robot.Database.Value;
+import org.usfirst.frc.team2473.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveStraightForward extends Command{
+	private double distance;
+	private static final double KP = .075;
+	private static final double KI = .003;
+	private static final double KD = 0;
+	
+	private double startingGyroValue;
+	private double integral;
+	private double lastProportion;
+	
+	public DriveStraightForward(double distance){
+		requires(Robot.driveTrain);
+		
+		this.distance = distance;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.sensorThread.resetEncoders();
+    	
+    	startingGyroValue = Database.getInstance().getValue(Value.GYRO);
+    	integral = 0;
+    	lastProportion = 0;
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double proportion = Database.getInstance().getValue(Value.GYRO) - startingGyroValue;
+    	integral += proportion;
+    	double derivative = proportion - lastProportion;
+    	double rotate = KP * proportion + KI*integral + KD*derivative;
+    	
+    	if(Math.abs(rotate) > .70){
+    		rotate = Math.signum(rotate) * .7;
+    	}
+    	
+    	Robot.driveTrain.driveArcade(.7, rotate);
+    	
+    	lastProportion = proportion;
+    	
+	
+    }
+
+	protected boolean isFinished() {
+		return Math.abs(Database.getInstance().getValue(Value.LEFT_ENCODER)) > distance;
+	}
+	
+	protected void end() {
+    	Robot.driveTrain.drive(0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
---
+++ src/org/usfirst/frc/team2473/robot/commands/Turn.java
@@ -0,0 +1,58 @@
+package org.usfirst.frc.team2473.robot.commands;
+
+import org.usfirst.frc.team2473.robot.Database;
+import org.usfirst.frc.team2473.robot.Robot;
+import org.usfirst.frc.team2473.robot.Database.Value;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Turn extends Command{
+	private double bearing;
+	
+	private static final double KP = .05;
+	private static final double KI = .003;
+	private static final double KD = 0;
+	
+	private double integral;
+	private double lastProportion;
+	
+	public Turn(double bearing){
+		requires(Robot.driveTrain);
+		
+		this.bearing = bearing;
+	}
+	
+	protected void initialize() {
+    	Robot.sensorThread.resetEncoders();
+    	Robot.sensorThread.resetGyro();
+    	
+    	integral = 0;
+    	lastProportion = 0;
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double proportion = Database.getInstance().getValue(Value.GYRO);
+    	integral += proportion;
+    	double derivative = proportion - lastProportion;
+    	double rotate = KP * proportion + KI*integral + KD*derivative;
+    	
+    	if(Math.abs(rotate) > .90){
+    		rotate = Math.signum(rotate) * .9;
+    	}
+    	
+    	Robot.driveTrain.driveArcade(0, rotate);
+    	
+    	lastProportion = proportion;
+    }
+
+	protected boolean isFinished() {
+		return Math.abs(Database.getInstance().getValue(Value.GYRO) - bearing) < 2 ;
+	}
+	
+	protected void end() {
+    	Robot.driveTrain.drive(0, 0);
+    }
+	
+}

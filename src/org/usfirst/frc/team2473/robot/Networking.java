860306384e4f7b87966695f5147a7c99766c8bd6
Rehan S. Durrani <rehansdurrani@gmail.com>
2017-02-14 12:18:28 -0800

    Made it so that Networking will block (for a while) on pi.

--- src/org/usfirst/frc/team2473/robot/Networking.java
+++ src/org/usfirst/frc/team2473/robot/Networking.java
@@ -1,115 +1,133 @@
 package org.usfirst.frc.team2473.robot;
 
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ConnectException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.channels.SocketChannel;
-import java.nio.channels.spi.SelectorProvider;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.usfirst.frc.team2473.robot.Database.Value;
-
 public class Networking extends Thread {
-	// MUST CHANGE ON COMPETITION DAY
-	private final String HOST = "10.60.38.97";
-	private final int PORT = 5811;
-	private final byte[] SEND = "CV()".getBytes(Charset.defaultCharset());
-	private char[] cbuf = new char[4096];
-	private SocketChannel socketChannel = null;
-	private Socket s = null;
-	private BufferedReader stdIn = null;
-	private OutputStream stdOut = null;
-	private Database d = Database.getInstance();
-	private final static int TIME_OUT = 500;
-	Value[] values = { Value.CV_DISTANCE, Value.CV_ANGLE_A, Value.CV_BEARING, Value.CV_L_OR_R, Value.CV_TIME_STAMP };
+    // MUST CHANGE ON COMPETITION DAY
+    private final String HOST = "10.19.92.233";
+    private final int PORT = 4444;
+    private final byte[] SEND = "CV()".getBytes(Charset.defaultCharset());
+    private char[] cbuf = new char[4096];
+    private Socket s = new Socket();
+    private BufferedReader stdIn = null;
+    private OutputStream stdOut = null;
+    private Database d = Database.getInstance();
+    private final static int TIME_OUT = 500;
+    Database.Value[] values = {Database.Value.CV_DISTANCE, Database.Value.CV_ANGLE_A, Database.Value.CV_BEARING, Database.Value.CV_L_OR_R, Database.Value.CV_TIME_STAMP};
 
-	static Networking instance;
-	static {
-		instance = new Networking();
-	}
+    static Networking instance;
 
-	public static Networking getInstance() {
-		return instance;
-	}
+    static {
+        instance = new Networking();
+    }
 
-	public void start() {
-		int i = 0;
-		try {
-			socketChannel = SocketChannel.open(new InetSocketAddress(HOST, PORT));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		while (!socketChannel.isConnected() && i < TIME_OUT) {
-			try {
-				socketChannel.connect(new InetSocketAddress(HOST, PORT));
-				d.setValue(Value.CV_PI_CONNECTED, 0);
-			} catch (Exception e) {
-				d.setValue(Value.CV_PI_CONNECTED, 1);
-			}
-			i++;
-		}
-		try {
-			s = socketChannel.socket();
-			stdIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			stdOut = s.getOutputStream();
-		} catch (IOException e) {
-			System.exit(0);
-		}
-		super.start();
-	}
+    public static Networking getInstance() {
+        return instance;
+    }
 
-	public void run() {
-		while (true) {
-			try {
-				synchronized (this) {
-					wait();
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			int i = 0;
-			while (!socketChannel.isConnected() && i < TIME_OUT) {
-				try {
-					socketChannel.connect(new InetSocketAddress(HOST, PORT));
-					d.setValue(Value.CV_PI_CONNECTED, 0);
-				} catch (Exception e) {
-					d.setValue(Value.CV_PI_CONNECTED, 1);
-				}finally{
-				
-					i++;
-				}
-				if(socketChannel.isConnected()){
-					s = socketChannel.socket();
-				}			}
-			update();
-		}
-	}
-
-	public void update() {
-		try {
-			stdOut.write(SEND);
-			stdIn.read(cbuf);
-			String st = String.copyValueOf(cbuf);
-			Matcher matcher = Pattern.compile("\\d*(\\.\\d*)").matcher(st);
-            for(Value v : values){
-            		matcher.find();
-            		d.setValue(v, Double.parseDouble(matcher.group()));
+    public void start() {
+        int i = 0;
+        System.out.println("Good morning!");
+        try {
+            System.out.println("About to make socket");
+            s.connect(new InetSocketAddress(HOST, PORT), 100);
+            System.out.println("Made socket");
+            s.setKeepAlive(true);
+            System.out.println("Keep alive");
+            d.setValue(Database.Value.CV_PI_CONNECTED, 0);
+            System.out.println("My connection has succeeded!");
+        } catch (Exception e1) {
+            d.setValue(Database.Value.CV_PI_CONNECTED, 1);
+            System.out.println("My connection has failed but I will go on.");
+        }
+        System.out.println("Before Loop!");
+        boolean b = false;
+        try{
+            b  = s.getLocalAddress().isReachable(100);
+        }catch (IOException exception){
+        }
+        while (!b && i < TIME_OUT) {
+            try {
+                s.connect(new InetSocketAddress(HOST, PORT), 100);
+                s.setKeepAlive(true);
+                d.setValue(Database.Value.CV_PI_CONNECTED, 0);
+                sleep(1000);
+                System.out.println("My connection has succeeded!");
+            } catch (Exception e) {
+                d.setValue(Database.Value.CV_PI_CONNECTED, 1);
+                System.out.println("My connection has failed but I will go on.");
             }
-		} catch (Exception e) {
-			d.setValue(Value.CV_PI_CONNECTED, 1);
-		}
-	}
+            i++;
+        }
+        System.out.println("Completed loop");
+        try {
+            stdIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            stdOut = s.getOutputStream();
+        } catch (IOException e) {
+            d.setValue(Database.Value.CV_PI_CONNECTED, 1);
+        }
+        super.start();
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println("Hello running run");
+            try {
+                synchronized (this) {
+                    wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Done waiting!");
+            int i = 0;
+            boolean b = false;
+            try{
+                b  = s.getLocalAddress().isReachable(100);
+            }catch (IOException exception){
+            }
+            while (!b && i < TIME_OUT) {
+                try {
+                    s.connect(new InetSocketAddress(HOST, PORT), 100);
+                    s.setKeepAlive(true);
+                    d.setValue(Database.Value.CV_PI_CONNECTED, 0);
+                    System.out.println("My connection has succeeded!");
+                    wait(1000);
+                } catch (Exception e) {
+                    d.setValue(Database.Value.CV_PI_CONNECTED, 1);
+                    System.out.println("My connection has failed but I will go on.");
+                } finally {
+                    i++;
+                }
+            }
+            System.out.println("Hello exited loop");
+            update();
+        }
+    }
+
+    public void update() {
+        try {
+            System.out.println("Hello update");
+            stdOut.write(SEND);
+            stdIn.read(cbuf);
+            String st = String.copyValueOf(cbuf);
+            Matcher matcher = Pattern.compile("\\d*(\\.\\d*)").matcher(st);
+            for (Database.Value v : values) {
+                matcher.find();
+                d.setValue(v, Double.parseDouble(matcher.group()));
+            }
+        } catch (Exception e) {
+            System.out.println("I am not doing so hot rn.");
+            d.setValue(Database.Value.CV_PI_CONNECTED, 1);
+        }
+    }
 }
